a2f4c4a5c726fd8c78cf88a3a0e522b93c836fbc
Tanishkaap <tanishkaap06@gmail.com>
2026-09-20 15:17:40 +0530

    Add Program 01 - Basic Single Inheritance
    Implemented Person and Student classes with constructors and display functions.

---
+++ Program_01/program01.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <utility>
+
+using namespace std;
+
+// Base class
+class Person
+{
+protected:
+    string name;
+
+public:
+    // Constructor of base class
+    explicit Person(string personName) : name(move(personName)) {}
+
+    // Function to display name
+    void displayName() const
+    {
+        cout << "Name: " << name << '\n';
+    }
+};
+
+// Derived class inheriting from Person
+class Student : public Person
+{
+private:
+    int rollNumber;
+
+public:
+    // Constructor of derived class
+    Student(string studentName, int roll)
+        : Person(move(studentName)), rollNumber(roll) {}
+
+    // Function to display student details
+    void displayStudent() const
+    {
+        displayName();
+        cout << "Roll Number: " << rollNumber << '\n';
+    }
+};
+
+int main()
+{
+    // Creating an object of Student class
+    Student student("Amit", 101);
+
+    // Displaying student details
+    student.displayStudent();
+
+    return 0;
+}
