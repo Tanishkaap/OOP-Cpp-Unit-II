e76919f055dad9c2b13b6cb69ea1220b96ab3613
Tanishkaap <tanishkaap06@gmail.com>
2026-09-26 18:14:38 +0530

    Program 08 - Add Constructor And Destructor
    Implement base and derived class with constructors and destructors.

---
+++ Program_08/program08.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+// Base class
+class Base
+{
+public:
+    Base()
+    {
+        cout << "Base constructor\n";
+    }
+
+    ~Base()
+    {
+        cout << "Base destructor\n";
+    }
+};
+
+// Derived class
+class Derived : public Base
+{
+public:
+    Derived()
+    {
+        cout << "Derived constructor\n";
+    }
+
+    ~Derived()
+    {
+        cout << "Derived destructor\n";
+    }
+};
+
+int main()
+{
+    // Creating derived object calls constructors
+    Derived object;
+
+    return 0;
+}
