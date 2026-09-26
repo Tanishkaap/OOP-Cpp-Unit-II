4c6a0e80b840a84fee54a4f1d71367a0c0231b6e
Tanishkaap <tanishkaap06@gmail.com>
2026-09-26 21:53:13 +0530

    Add Unit I Program 02 - If Else

---
+++ Unit_01/Program_02/program02.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Marks obtained by the student
+    int marks = 45;
+
+    // Check whether the student has passed
+    if (marks >= 40)
+    {
+        cout << "Pass";
+    }
+    else
+    {
+        cout << "Fail";
+    }
+
+    return 0;
+}
