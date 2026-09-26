1debe126877135266f544fa8183414769d1827fa
Tanishkaap <tanishkaap06@gmail.com>
2026-09-26 21:53:09 +0530

    Add Unit I Program 01 - Basic Data Types

---
+++ Unit_01/Program_01/program01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Store the basic details of a student
+    int roll = 101;
+    char grade = 'A';
+    float fee = 12500.50;
+
+    // Display the stored values
+    cout << "Roll No: " << roll << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Fee: " << fee << endl;
+
+    return 0;
+}
