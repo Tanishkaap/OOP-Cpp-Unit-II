0848ce322b5a62ce823921a7c69b516b7863ae69
Tanishkaap <tanishkaap06@gmail.com>
2026-09-26 21:55:22 +0530

    Add Unit III Program 07 - Friend Operator Overloading

---
+++ Unit_03/Program_07/program07.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+private:
+    int real;
+    int imaginary;
+
+public:
+    Complex(int realPart = 0, int imaginaryPart = 0)
+        : real(realPart), imaginary(imaginaryPart) {}
+
+    // Friend function supports an int on the left side of +
+    friend Complex operator+(int value, const Complex& number);
+
+    void display() const
+    {
+        cout << real;
+        if (imaginary >= 0)
+            cout << " + ";
+        else
+            cout << " - ";
+        cout << (imaginary >= 0 ? imaginary : -imaginary) << "i\n";
+    }
+};
+
+Complex operator+(int value, const Complex& number)
+{
+    return Complex(value + number.real, number.imaginary);
+}
+
+int main()
+{
+    Complex number(2, 3);
+    Complex result = 10 + number;
+
+    cout << "Result: ";
+    result.display();
+
+    return 0;
+}
