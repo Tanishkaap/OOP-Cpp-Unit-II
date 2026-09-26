e79e7d97fa1421b341ae8c05063ccdf666204dfb
Tanishkaap <tanishkaap06@gmail.com>
2026-09-26 18:16:06 +0530

    Add Program 09 - Parameterized Base Constructor

---
+++ Program_09/program09.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <utility>
+
+using namespace std;
+
+// Base class
+class Person
+{
+protected:
+    string name;
+
+public:
+    explicit Person(string personName)
+        : name(move(personName)) {}
+};
+
+// Student inherits from Person
+class Student : public Person
+{
+private:
+    int rollNumber;
+
+public:
+    // Passing student name to the base class constructor
+    Student(string studentName, int roll)
+        : Person(move(studentName)), rollNumber(roll) {}
+
+    void display() const
+    {
+        cout << "Name: " << name << '\n';
+        cout << "Roll Number: " << rollNumber << '\n';
+    }
+};
+
+int main()
+{
+    Student student("Kiran", 24);
+
+    student.display();
+
+    return 0;
+}
